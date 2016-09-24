039e04c813383b6d4b032cac607293e8884ec9af
Saskia Heckele <saskia.heckele@pentasys.de>
2016-09-24 13:54:54 +0200

    Moved newLocation to day.html

--- src/main/java/com/travel/photoplanner/controller/DayController.java
+++ src/main/java/com/travel/photoplanner/controller/DayController.java
@@ -3,6 +3,7 @@ package com.travel.photoplanner.controller;
 import com.travel.photoplanner.entity.Day;
 import com.travel.photoplanner.entity.Location;
 import com.travel.photoplanner.service.DayService;
+import com.travel.photoplanner.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +18,15 @@ public class DayController {
     @Autowired
     private DayService dayService;
 
+    @Autowired
+    private TripService tripService;
+
     @RequestMapping("/day")
-    public ModelAndView day(@RequestParam(value = "id") int id,
+    public ModelAndView day(@RequestParam(value = "trip") int tripId,
+                            @RequestParam(value = "id") int id,
                             ModelAndView modelAndView) {
 
+        modelAndView.addObject("trip", tripService.findTripById(tripId));
         modelAndView.addObject("day", dayService.findDayById(id));
         modelAndView.setViewName("day");
 
